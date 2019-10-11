9599a03941f0b78442cc02f1785d28e8684aa6f1
tangert <tangert@tom-w8>
2019-10-10 21:04:33 -0400

    Merge from master

--- app/includes/config.php
+++ app/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (C) 2016 Alexander Marston (alexander.marston@gmail.com)
+ * Copyright (C) 2019 Alexander Marston (alexander.marston@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,14 +17,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// Disable error reporting to screen
+/*ini_set('display_errors', 0);
+error_reporting(E_ALL);*/
+
 // Set the default system Timezone
 date_default_timezone_set('Europe/London');
 
 // Path of vnstat
 $vnstat_bin_dir = '/usr/bin/vnstat';
-
-// Uncomment to override default config file
-//$vnstat_config = '/etc/vnstat.conf';
 
 // Set to true to set your own interfaces
 $use_predefined_interfaces = false;
@@ -34,10 +35,4 @@ if ($use_predefined_interfaces == true) {
 
     $interface_name['eth0'] = "Internal #1";
     $interface_name['eth1'] = "Internal #2";
-} else {
-    $interface_list = getVnstatInterfaces($vnstat_bin_dir);
-
-    foreach ($interface_list as $interface) {
-        $interface_name[$interface] = $interface;
-    }
 }
